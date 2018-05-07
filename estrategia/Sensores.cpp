a7c886cd32f08ad4ffe1dec6dbb34f73b958c0c2
robotica@sc111497 <robotica@sc111497>
2018-05-07 11:12:04 -0300

    Adicionada classes no cpp e h

--- estrategia/Sensores.cpp
+++ estrategia/Sensores.cpp
@@ -2,5 +2,21 @@
 #include "Sensores.h"
 #define CINZA 50
 
+void Sensores:: brancoBrancoBrancoBranco(){
 
+}
+
+void Sensores:: brancoBrancoBrancoPreto(){}
+
+void Sensores:: brancoBrancoPretoBranco(){}
+
+void Sensores:: brancoBrancoPretoPreto(){}
+
+void Sensores:: brancoPretoBrancoBranco(){}
+
+void Sensores:: brancoPretoPretoBranco(){}
+
+void Sensores:: pretoPretoPretoBranco(){}
+
+void Sensores:: senao(){}
 
--- estrategia/Sensores.h
+++ estrategia/Sensores.h
@@ -3,12 +3,19 @@ class Sensores{
 public:
 
 void brancoBrancoBrancoBranco();
+
 void brancoBrancoBrancoPreto();
+
 void brancoBrancoPretoBranco();
+
 void brancoBrancoPretoPreto();
+
 void brancoPretoBrancoBranco();
+
 void brancoPretoPretoBranco();
+
 void pretoPretoPretoPreto();
+
 void senao();
 
 
