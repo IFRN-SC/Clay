e25b01c973ea31999686676b44856beed37ed6e3
robotica@sc111497 <robotica@sc111497>
2018-05-10 16:54:25 -0300

    Adicionada as condições nas funções

--- estrategia/Sensores.cpp
+++ estrategia/Sensores.cpp
@@ -2,21 +2,180 @@
 #include "Sensores.h"
 #define CINZA 50
 
-void Sensores:: brancoBrancoBrancoBranco(){
+double valorMaisEsq = lerSensorLinhaMaisEsq();
+double valorEsq = lerSensorLinhaEsq();
+double valorDir = lerSensorLinhaDir();
+double valorMaisDir = lerSensorLinhaMaisDir();
+
+
+bool Sensores:: brancoBrancoBrancoBranco(){
+ int contValor;
+ if(valorMaisEsq > CINZA){
+   contValor++;
+}
+ else if(valorEsq > CINZA){
+  contValor++;
+}
+ else if(valorDir > CINZA){
+   contValor++;
+}
+ else if(valorMaisDir > CINZA){
+  contValor++;
+}
+
+  if(contValor==4){
+    return true;
+}
+  else{
+   return false;
+}
 
 }
 
-void Sensores:: brancoBrancoBrancoPreto(){}
+bool Sensores:: brancoBrancoBrancoPreto(){
+ int contValor;
+ if(valorMaisEsq > CINZA){
+   contValor++;
+}
+ else if(valorEsq > CINZA){
+  contValor++;
+}
+ else if(valorDir > CINZA){
+   contValor++;
+}
+ else if(valorMaisDir <= CINZA){
+  contValor++;
+}
 
-void Sensores:: brancoBrancoPretoBranco(){}
+  if(contValor==4){
+    return true;
+}
+  else{
+   return false;
+}
 
-void Sensores:: brancoBrancoPretoPreto(){}
+}
 
-void Sensores:: brancoPretoBrancoBranco(){}
+bool Sensores:: brancoBrancoPretoBranco(){
+ int contValor;
+ if(valorMaisEsq > CINZA){
+   contValor++;
+}
+ else if(valorEsq > CINZA){
+  contValor++;
+}
+ else if(valorDir <= CINZA){
+   contValor++;
+}
+ else if(valorMaisDir > CINZA){
+  contValor++;
+}
 
-void Sensores:: brancoPretoPretoBranco(){}
+  if(contValor==4){
+    return true;
+}
+  else{
+   return false;
+}
 
-void Sensores:: pretoPretoPretoBranco(){}
+}
 
-void Sensores:: senao(){}
+bool Sensores:: brancoBrancoPretoPreto(){
+ int contValor;
+ if(valorMaisEsq > CINZA){
+   contValor++;
+}
+ else if(valorEsq > CINZA){
+  contValor++;
+}
+ else if(valorDir <= CINZA){
+   contValor++;
+}
+ else if(valorMaisDir <= CINZA){
+  contValor++;
+}
+
+  if(contValor==4){
+    return true;
+}
+  else{
+   return false;
+}
+
+}
+
+bool Sensores:: brancoPretoBrancoBranco(){
+ int contValor;
+ if(valorMaisEsq > CINZA){
+   contValor++;
+}
+ else if(valorEsq <= CINZA){
+  contValor++;
+}
+ else if(valorDir > CINZA){
+   contValor++;
+}
+ else if(valorMaisDir > CINZA){
+  contValor++;
+}
+
+  if(contValor==4){
+    return true;
+}
+  else{
+   return false;
+}
+
+}
+
+bool Sensores:: brancoPretoPretoBranco(){
+ int contValor;
+ if(valorMaisEsq > CINZA){
+   contValor++;
+}
+ else if(valorEsq <= CINZA){
+  contValor++;
+}
+ else if(valorDir <= CINZA){
+   contValor++;
+}
+ else if(valorMaisDir > CINZA){
+  contValor++;
+}
+
+  if(contValor==4){
+    return true;
+}
+  else{
+   return false;
+}
+
+}
+
+bool Sensores:: pretoBrancoBrancoBranco(){
+ int contValor;
+ if(valorMaisEsq <= CINZA){
+   contValor++;
+}
+ else if(valorEsq > CINZA){
+  contValor++;
+}
+ else if(valorDir > CINZA){
+   contValor++;
+}
+ else if(valorMaisDir > CINZA){
+  contValor++;
+}
+
+  if(contValor==4){
+    return true;
+}
+  else{
+   return false;
+}
+
+}
+
+bool Sensores:: senao(){
+}
 
