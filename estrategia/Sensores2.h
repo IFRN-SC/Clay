a51985a61dbac46e6476f6743236cb82bcbbaa00
robotica@sc111481 <robotica@sc111481>
2018-05-29 16:55:24 -0300

    Classes novas

---
+++ estrategia/Sensores2.h
@@ -0,0 +1,18 @@
+class Sensores2{
+
+
+double valorEsq;
+double valorDir;
+
+public:
+
+bool brancoBranco();
+
+bool brancoPreto();
+
+bool pretoBranco();
+
+bool pretoPreto();
+
+void atualizar();
+};
