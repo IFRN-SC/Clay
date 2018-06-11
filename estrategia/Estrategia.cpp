f49679dcc623df1c15add3ae4f2fdc37b93c9422
robotica@sc111481 <robotica@sc111481>
2018-06-11 18:25:12 -0300

    remoção de linhas

--- estrategia/Estrategia.cpp
+++ estrategia/Estrategia.cpp
@@ -1,45 +1,26 @@
 #include "Estrategia.h"
 #include "robo_hardware2.h"
 
-void Estrategia::executa(){
-	seguirLinha();
+void Estrategia::executa() {
+  seguirLinha();
 }
-void Estrategia::seguirLinha(){
+void Estrategia::seguirLinha() {
 
-	if(sensores.brancoBrancoBrancoBranco()){
-
-		robo.acionarMotores(50,50);
-
-	}else if(sensores.brancoBrancoBrancoPreto()){
-
-		robo.acionarMotores(50,-50);
-
-	}else if(sensores.brancoBrancoPretoBranco()){
-
-		robo.acionarMotores(-50,50);
-
-	}else if(sensores.brancoBrancoPretoPreto()){
-
-		robo.acionarMotores(50,-50);
-	}else if(sensores.brancoPretoBrancoBranco()){
-
-		robo.acionarMotores(50,-50);
-
-	}else if(sensores.brancoPretoPretoBranco()){
-
-		robo.acionarMotores(-50,-50);
-
-	}else if(sensores.pretoBrancoBrancoBranco()){
-
-		robo.acionarMotores(-50,-50);
-
-	}else{
-    
-		robo.acionarMotores(50,-50);
+  if (sensores.brancoBrancoBrancoBranco()) {
+    robo.acionarMotores(45, 45);
+  } else if (sensores.brancoPretoBrancoBranco()) {
+    robo.acionarMotores(-50, 50);
+  } else if (sensores.brancoBrancoPretoBranco()) {
+    robo.acionarMotores(50, -50);
+  } else if (sensores.brancoBrancoBrancoPreto()) {
+    robo.acionarMotores(50, -60);
+  } else if (sensores.pretoBrancoBrancoBranco()) {
+    robo.acionarMotores(-60, 50);
+  } else if (sensores.pretoPretoPretoPreto()) {
+    robo.acionarMotores(50, 50);
+  }
 
 }
-
-}
-void Estrategia::verde(){}
-void Estrategia::sala3(){}
-void Estrategia::desviarObstaculo(){}
+void Estrategia::verde() {}
+void Estrategia::sala3() {}
+void Estrategia::desviarObstaculo() {}
