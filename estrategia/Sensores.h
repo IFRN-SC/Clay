ee530dfafe23e9f492207e2f10a317aefec0e6ea
robotica@sc111485 <robotica@sc111485>
2018-06-15 17:52:00 -0300

    Importação da classe calibracao. Criacao do objeto de calibracao

--- estrategia/Sensores.h
+++ estrategia/Sensores.h
@@ -1,9 +1,12 @@
+#include "Calibracao.h"
 class Sensores{
 
-double valorMaisEsq;
+/*double valorMaisEsq;
 double valorEsq;
 double valorDir;
 double valorMaisDir;
+*/
+Calibracao calibracao;
 
 public:
 
@@ -23,5 +26,5 @@ bool pretoBrancoBrancoBranco();
 
 bool pretoPretoPretoPreto();
 
-void atualizar();
+//void atualizar();
 };
