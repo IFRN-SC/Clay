229cb89abbb81024d68446cae91a1b8be07e43e5
robotica@sc111532 <robotica@sc111532>
2018-07-31 15:21:56 -0300

    Adicionado calibração

---
+++ estrategia/Calibracao.h
@@ -0,0 +1,27 @@
+class Calibracao{
+
+double valorMaisEsq;
+double valorEsq;
+double valorDir;
+double valorMaisDir;
+
+public:
+
+bool bValorMaisEsq();
+bool pValorMaisEsq();
+
+bool bValorEsq();
+bool pValorEsq();
+
+bool bValorDir();
+bool pValorDir();
+
+bool bValorMaisDir();
+bool pValorMaisDir();
+
+void atualizarVME();
+void atualizarVE();
+void atualizarVD();
+void atualizarVMD();
+
+};
