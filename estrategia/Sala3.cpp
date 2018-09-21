b3c0d9819a8aa600719c92625eb2c5ea717da2de
robotica@sc111497 <robotica@sc111497>
2018-09-21 17:36:20 -0300

    Alteracao teste da sala3

--- estrategia/Sala3.cpp
+++ estrategia/Sala3.cpp
@@ -15,31 +15,29 @@ void Sala3:: rampa() {
   while (!parar) {
 
     if (sensores.brancoBrancoBrancoBranco()) {
-      movimento.frenmed();
+      movimento.superfrent();
     }
     else if (sensores.brancoPretoBrancoBranco()) {
-      movimento.dirr();
-    }
-    else if (sensores.brancoBrancoPretoBranco()) {
       movimento.esqq();
     }
+    else if (sensores.brancoBrancoPretoBranco()) {
+      movimento.dirr();
+    }
     else if (sensores.brancoBrancoBrancoPreto()) {
-      movimento.exesq();
+      movimento.exdir();
     }
     else if (sensores.pretoBrancoBrancoBranco()) {
-      movimento.exdir();
+      movimento.exesq();
     }
     else if (sensores.pretoPretoPretoPreto()) {
       movimento.superfrent();
       delay(900);
-
-
       movimento.parar();
       while (true);
       parar = true;
     }
     else if (sensores.pretoPretoBrancoBranco()) {
-      robo.acionarMotores(80, 100);
+      robo.acionarMotores(80, -60);
       delay(1000);
 
       movimento.parar();
@@ -47,7 +45,7 @@ void Sala3:: rampa() {
       parar = true;
     }
     else if (sensores.brancoBrancoPretoPreto()) {
-      robo.acionarMotores(100, 80);
+      robo.acionarMotores(-60, 80);
       delay(1000);
 
 
